73ea51a8ea6b6d91de34756235cdf69f455de09a
Gloriaamandagii <glorriiaamandagii@gmail.com>
2025-02-11 14:14:12 +0800

    basic react native

--- App.tsx
+++ App.tsx
@@ -1,7 +1,41 @@
-import {Text} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 
 const App = () => {
-  return <Text>Hello World. Halo halo</Text>;
+  return (
+    <View style={styles.wrapper}>
+      <View style={styles.container}>
+        <Text style={styles.title1}>Basic React Native</Text>
+      </View>
+
+      <View style={styles.container}>
+        <Text style={styles.title2}> Basic React Native 2</Text>
+      </View>
+    </View>
+  );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+  },
+  container: {
+    backgroundColor: 'red',
+    borderColor: 'black',
+    borderWidth: 10,
+    padding: 50,
+    margin: 20,
+    borderRadius: 40,
+  },
+  title1: {
+    fontSize: 30,
+    color: 'yellow',
+    fontWeight: '800',
+  },
+  title2: {
+    fontSize: 30,
+    color: 'blue',
+    fontWeight: '800',
+  },
+});
