28335edad33113b293238f6961f45ee8528cb1da
sergiopoli77 <s22210048@student.unklab.ac.id>
2025-02-27 14:02:32 +0800

    add sapce-between

--- Flexbox.tsx
+++ Flexbox.tsx
@@ -17,7 +17,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
   },
   redContainer: {
     backgroundColor: 'red',
