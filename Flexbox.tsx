4cf77ca8bacf9badd778519843eeeaac10846ec6
sergiopoli77 <s22210048@student.unklab.ac.id>
2025-02-27 14:02:57 +0800

    add space-around

--- Flexbox.tsx
+++ Flexbox.tsx
@@ -17,7 +17,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    justifyContent: 'space-between',
+    justifyContent: 'space-around',
   },
   redContainer: {
     backgroundColor: 'red',
