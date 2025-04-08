58c6385542fa16161971e0341f8d06c50ea45d6f
sergiopoli77 <s22210048@student.unklab.ac.id>
2025-04-08 14:20:28 +0800

    add splashscreen

--- index.js
+++ index.js
@@ -4,15 +4,6 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-import Exercise2 from './Exercise2';
-import Exercise3 from './Exercise3';
-import Flexbox from './Flexbox';
-import Exercise4 from './Exercise4';
-import Signin from './Signin';
-import Exercise5 from './Exercise5/Exercise5';
-import Exercise6 from './Exercise6/Exercise6';
-import RestAPI from './RestAPI';
-import UserList from './Exercise7/UserList';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => UserList);
+AppRegistry.registerComponent(appName, () => App);
