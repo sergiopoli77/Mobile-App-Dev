95722b10d37a556f6ed19272c99a85fde73c8a88
sergiopoli77 <s22210048@student.unklab.ac.id>
2025-04-10 14:06:38 +0800

    slicing sign in page

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,14 +1,45 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import Header from '../../Components/molecules/Header';
+import TextInput from '../../Components/molecules/TextInput';
+import Button from '../../Components/atoms/Button';
+import Gap from '../../Components/atoms/Gap';
 
-const Signin = () => {
+const SignIn = () => {
   return (
-    <View>
-      <Text>Sign In</Text>
+    <View style={styles.pageContainer}>
+      <Header title="Sign In" />
+      <View style={styles.contentContainer}>
+        <Gap height={26} />
+        <TextInput
+          label="Email Address"
+          placeholder="Type your email address"
+        />
+        <Gap height={16} />
+        <TextInput label="Password" placeholder="Type your password" />
+        <Gap height={24} />
+        <Button label="Sign In" />
+        <Gap height={12} />
+        <Button
+          label="Create New Account"
+          color="#8D92A3"
+          textColor="#FFFFFF"
+        />
+      </View>
     </View>
   );
 };
 
-export default Signin;
+export default SignIn;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  pageContainer: {
+    flex: 1,
+  },
+  contentContainer: {
+    backgroundColor: '#FFFFFF',
+    marginTop: 24,
+    flex: 1,
+    paddingHorizontal: 24,
+  },
+});
